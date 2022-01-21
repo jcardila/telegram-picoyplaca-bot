006fba43b018a4a8c2f726002ae539aaa4d7c26e
jcardila <andrea.rueda@kellogg.northwestern.edu>
2022-01-21 23:55:27 +0000

    actualizada URL con la que da heroku

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ def getMessage():
 @server.route("/")
 def webhook():
     bot.remove_webhook()
-    bot.set_webhook(url='https://your_heroku_project.com/' + TOKEN)
+    bot.set_webhook(url='https://telegram-gustavo-bot.herokuapp.com/' + TOKEN)
     return "!", 200
 
 
