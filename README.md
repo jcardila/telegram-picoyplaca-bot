6318198ba0e249de9ee5ee8be260a5008f4c7744
jcardila <andrea.rueda@kellogg.northwestern.edu>
2022-01-21 21:17:19 +0000

    documentado y eliminar variable sin uso

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # telegram-gustavo-bot
+
+Ref: https://www.youtube.com/watch?v=NwBWW8cNCP4
+
+API_KEY Se configura como variable de ambiente en replit. Video lo hace crando archivo .env pero replit dice que eso está descontinuado
--- main.py
+++ main.py
@@ -2,15 +2,13 @@ import os
 import telebot
 import random
 
-API_KEY = os.environ['API_KEY']
+API_KEY = os.environ['API_KEY'] 
 bot = telebot.TeleBot(API_KEY)
 
 frases = ["Ni idea!", "Se lo comió el mar..", u"\U0001F937\u200D\u2642\uFE0F", "Ay mi niño..", "Ya no me acuerdo de él :(", "Ese es el monito ese?", "Voló..", "No sufras tanto, por favor", "Me gustas cuando callas porque estás como ausente", "Dice que trabaja..", "Obvio está borracho", "Lo perdimos amigo, lo perdimos.." ]
 
 @bot.message_handler(commands=['DondeEstaGustavo'])
 def dondeEstaGustavo(message):
-
-  cant_frases = len(frases)
 
   msj = random.choice(frases)
   bot.reply_to(message, msj)
