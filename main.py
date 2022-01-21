97ce555498ccf5dbf32191c94ffc939048d9e70f
jcardila <andrea.rueda@kellogg.northwestern.edu>
2022-01-21 21:14:43 +0000

    funcionando

---
+++ main.py
@@ -0,0 +1,32 @@
+import os
+import telebot
+import random
+
+API_KEY = os.environ['API_KEY']
+bot = telebot.TeleBot(API_KEY)
+
+frases = ["Ni idea!", "Se lo comió el mar..", u"\U0001F937\u200D\u2642\uFE0F", "Ay mi niño..", "Ya no me acuerdo de él :(", "Ese es el monito ese?", "Voló..", "No sufras tanto, por favor", "Me gustas cuando callas porque estás como ausente", "Dice que trabaja..", "Obvio está borracho", "Lo perdimos amigo, lo perdimos.." ]
+
+@bot.message_handler(commands=['DondeEstaGustavo'])
+def dondeEstaGustavo(message):
+
+  cant_frases = len(frases)
+
+  msj = random.choice(frases)
+  bot.reply_to(message, msj)
+
+@bot.message_handler(commands=['PingGustavo'])
+def pingGustavo(message):
+
+  bot.send_message(message.chat.id, "Ping Gustavo!")
+  bot.send_message(message.chat.id, "Ping Gustavo!")
+  bot.send_message(message.chat.id, "Ping Gustavo!")
+  bot.send_message(message.chat.id, "Ping Gustavo!")
+  bot.send_message(message.chat.id, "Ping Gustavo!")
+  bot.send_message(message.chat.id, "Ping Gustavo!")
+  bot.send_message(message.chat.id, "Ping Gustavo!")
+  bot.send_message(message.chat.id, "Ping Gustavo!")
+  bot.send_message(message.chat.id, "Ping Gustavo!")
+  bot.send_message(message.chat.id, "Ping Gustavo!")
+
+bot.polling()
