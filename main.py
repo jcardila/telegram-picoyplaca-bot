f291b2d04e82b94bbfc1d2c556a713781dbcba74
jcardila <juancamiloardila@gmail.com>
2022-07-01 20:30:17 +0000

    Actualizado PyP Bmga Trimestre 3 + mensaje de datos desactualizados

--- main.py
+++ main.py
@@ -17,27 +17,31 @@ weekday_hoy = fecha_hoy.isoweekday()  #Monday is 1 and Sunday is 7
 day_hoy = fecha_hoy.day
 
 #Pico y placa actual Bucaramanga
+vigencia_bmga = "2022-08-30"
 horario_bmga_entre_semana = "De 6am a 8pm"
 regla_bmga_entre_semana = {
-  1: "5 y 6", #Lunes
-  2: "7 y 8", #Martes
-  3: "9 y 0", #Miercoles
-  4: "1 y 2", #Jueves
-  5: "3 y 4"} #Viernes
+  1: "7 y 8", #Lunes
+  2: "9 y 0", #Martes
+  3: "1 y 2", #Miercoles
+  4: "3 y 4", #Jueves
+  5: "5 y 6"} #Viernes
 
 horario_bmga_sabado = "De 9am a 1pm"
 regla_bmga_sabado = {
   "2022-04-16": "1 y 2",
-  "2022-04-23": "3 y 4",
-  "2022-04-30": "5 y 6",
-  "2022-05-07": "7 y 8",
-  "2022-05-14": "9 y 0",
-  "2022-05-21": "1 y 2",
-  "2022-05-28": "3 y 4",
-  "2022-06-04": "5 y 6",
-  "2022-06-11": "7 y 8",
-  "2022-06-18": "9 y 0",
-  "2022-06-25": "1 y 2"
+  "2022-07-02": "3 y 4",
+  "2022-07-09": "5 y 6",
+  "2022-07-16": "7 y 8",
+  "2022-07-23": "9 y 0",
+  "2022-07-30": "1 y 2",
+  "2022-08-06": "3 y 4",
+  "2022-08-13": "5 y 6",
+  "2022-08-20": "7 y 8",
+  "2022-08-27": "9 y 0",
+  "2022-09-03": "1 y 2",
+  "2022-09-10": "3 y 4",
+  "2022-09-17": "5 y 6",
+  "2022-09-24": "1 y 2"
 }
 
 #Pico y placa actual Bogota
@@ -49,12 +53,15 @@ horario_bog_entre_semana = "De 6am a 9pm"
 @bot.message_handler(commands=['picoyplacabmgahoy'])
 def picoyplacabmgahoy(message):
 
-  if weekday_hoy == 7:    #Domingo
-    msj = f'\U0001F6D1 Hola! En Bucaramanga hoy no hay Pico y Placa \n'
-  elif weekday_hoy == 6:  #Sabado
-    msj = f'\U0001F6D1 Hola! En Bucaramanga hoy tiene Pico y Placa *{regla_bmga_sabado[fecha_hoy_str]}* \n Horario: {horario_bmga_sabado}'
-  else:                   #Entre semana
-    msj = f'\U0001F6D1 Hola! En Bucaramanga hoy tiene Pico y Placa *{regla_bmga_entre_semana[weekday_hoy]}* \n Horario: {horario_bmga_entre_semana}'
+  if fecha_hoy > vigencia_bmga:
+    msj = f'\U0001F6D1 Los datos del pico y placa de Bucaramanga están desactualizados! \n'
+  else:
+    if weekday_hoy == 7:    #Domingo
+      msj = f'\U0001F6D1 Hola! En Bucaramanga hoy no hay Pico y Placa \n'
+    elif weekday_hoy == 6:  #Sabado
+      msj = f'\U0001F6D1 Hola! En Bucaramanga hoy tiene Pico y Placa *{regla_bmga_sabado[fecha_hoy_str]}* \n Horario: {horario_bmga_sabado}'
+    else:                   #Entre semana
+      msj = f'\U0001F6D1 Hola! En Bucaramanga hoy tiene Pico y Placa *{regla_bmga_entre_semana[weekday_hoy]}* \n Horario: {horario_bmga_entre_semana}'
   
   bot.send_message(message.chat.id, msj)
 
